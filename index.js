ad80317e6579d7f07211a65905fe8affedfcfad9
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-09-20 16:34:26 +0530

    added code

---
+++ index.js
@@ -0,0 +1,36 @@
+
+
+object={
+    "Paris":"Skopj",
+    "Zurich":"Amsterdam",
+    "Prague":"Zurich",
+    "Barcelona":"Berlin",
+    "Kiev": "Prague",
+    "Skojp":"Paris",
+    "Amsterdam":"Barcelona",
+    "Berlin":"Amsterdam",
+}
+
+visited_city={
+   "Kiev":false,
+   "Amsterdam" :false,
+   "Zurich":false,
+   "Prague":false,
+   "Berlin":false,
+   "Barcelona":false,
+}
+
+count=0
+
+cur_city="Kiev"
+ans=""
+
+while(count<6){
+    count++
+    visited_city[cur_city]=true
+    ans=ans+cur_city 
+    ans=ans+"->"
+    cur_city=object[cur_city]
+}
+
+console.log(ans)
